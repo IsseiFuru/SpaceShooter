224058f6b130d86944cfa108f47ba75cb84440e6
Issei <menmaman53@gmail.com>
2022-12-09 15:59:47 +0900

    add Enemy

---
+++ G_Shooter/Intermediate/Source/G_Shooter.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class G_Shooter : ModuleRules
+{
+	public G_Shooter(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
